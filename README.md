468e8bbdd8fc1a8a75d4507bf5d2d33efa1ad2ee
Bangdayeon <dybang00@gmail.com>
2025-05-14 00:07:44 +0900

    Feat: 데이터 무효화+리패칭으로 게시글 추가 및 새로고침 실습

--- README.md
+++ README.md
@@ -19,7 +19,7 @@
 - [x] 2. QueryClient 생성 및 Provider 설정
 - [x] 3. useQuery 훅을 사용한 fetching
 - [x] 4. useMutation 훅을 사용한 Mutations
-- [ ] 5. 쿼리 무효화 및 리패칭
+- [x] 5. 쿼리 무효화 및 리패칭
 - [ ] 6. 기타 기능들(enabled 옵션, select 옵션, staleTime/cacheTime 옵션)
 - [ ] 7. React Query Devtools
 
--- src/components/PostList.tsx
+++ src/components/PostList.tsx
@@ -1,18 +1,44 @@
 import React from 'react';
-import { useQuery } from '@tanstack/react-query';
-import { fetchPosts } from "../api/posts.ts";
+import { useQuery, useMutation, useQueryClient } from '@tanstack/react-query';
+import { fetchPosts, createPost } from "../api/posts.ts";
 import { Link } from "react-router-dom";
 
+
 export function PostList() {
+    const queryClient = useQueryClient();
+
+    // 게시글 목록 가져오기
     const { data, isLoading, error } = useQuery({
         queryKey: ['posts'],
         queryFn: fetchPosts,
     });
 
+    // 게시글 추가 mutation
+    const mutation = useMutation({
+        mutationFn: createPost,
+        onSuccess: () => {
+            // 게시글 추가후 posts 쿼리 무효화 및 (자동)리패치
+            queryClient.invalidateQueries({queryKey:['posts']});
+            alert('Post added successfully!👏');
+        },
+    });
+
+    const handleAddPost = () => {
+        const newPost = {
+            title: 'New Post',
+            body: 'This is a new post.',
+            userId: 1,
+        };
+        mutation.mutate(newPost);
+    };
+
     if (isLoading) return <p>Loading...</p>;
     if (error instanceof Error) return <p>error: {error.message}</p>;
 
     return (
+        <div>
+            <h1>Post List</h1>
+            <button onClick={handleAddPost}>Add Post</button>
         <ul>
             {data.slice(0, 10).map((post: any) => (
                 <li key={post.id}>
@@ -20,5 +46,6 @@ export function PostList() {
                 </li>
             ))}
         </ul>
+        </div>
     )
 }
