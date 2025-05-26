246df7bacf2fd0984ac77da5fe86ed74ab70f0d4
Bangdayeon <dybang00@gmail.com>
2025-05-26 15:51:19 +0900

    Feat: axios-POST로 게시글 추가하도록 수정

--- src/api/posts.ts
+++ src/api/posts.ts
@@ -15,16 +15,6 @@ export const createPost = async (newPost: {
   body: string;
   userId: number;
 }) => {
-  const response = await fetch("https://jsonplaceholder.typicode.com/posts", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(newPost),
-  });
-
-  if (!response.ok) {
-    throw new Error("failed to create post");
-  }
-  return response.json();
+  const res = await api.post("/posts", newPost);
+  return res.data;
 };
